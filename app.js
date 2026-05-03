b304c02cd8e9badb9c8758be474d9a1fabeb4d67
SriPrakash-Sky <sriprakash@skyraan.com>
2026-05-03 18:45:15 +0530

    multer changes

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ import rateLimit from "express-rate-limit";
 import { connectDB } from "./config/db.js";
 import router from "./routes/index.js";
 import swaggerUi from "swagger-ui-express";
+import path from "path";
 import swaggerFile from "./swagger/swagger_output.json" with { type: "json" };
 dotenv.config();
 
@@ -22,7 +23,7 @@ app.use(
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(helmet());
-app.use("/uploads", express.static("uploads"));
+app.use("/uploads", express.static(path.join(process.cwd(), "uploads")));
 // app.use(
 //   rateLimit({
 //     windowMs: 15 * 60 * 1000,
--- config/multer.js
+++ config/multer.js
@@ -1,9 +1,17 @@
 import multer from "multer";
 import path from "path";
+import fs from "fs";
+
+const uploadDir = path.join(process.cwd(), "uploads");
+
+// ✅ create folder if not exists
+if (!fs.existsSync(uploadDir)) {
+  fs.mkdirSync(uploadDir, { recursive: true });
+}
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, "uploads/");
+    cb(null, uploadDir); // use absolute path
   },
   filename: (req, file, cb) => {
     cb(null, Date.now() + path.extname(file.originalname));
